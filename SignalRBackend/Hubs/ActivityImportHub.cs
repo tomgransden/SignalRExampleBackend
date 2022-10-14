fcb63d06fd3addde209699b653a798a37821641b
Thomas Gransden <thomas.gransden@slimmingworld.co.uk>
2022-10-14 21:28:27 +0100

    first commit

---
+++ SignalRBackend/Hubs/ActivityImportHub.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.SignalR;
+namespace SignalRBackend.Hubs
+{
+    public class ActivityImportHub : Hub
+    {
+        public Task Register() =>
+    Clients.Client(Context.ConnectionId)
+           .SendAsync("onRegister", true);
+    }
+}
+
---
+++ SignalRBackend/Program.cs
@@ -0,0 +1,27 @@
+﻿using SignalRBackend.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddSignalR().AddAzureSignalR("Endpoint=https://tgexamplesignalr.service.signalr.net;AccessKey=lV8Gwks+PvbVuSzT+la08zNI8C3enLZBx6UxQO9UuH4=;Version=1.0;");
+
+var app = builder.Build();
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHub<ActivityImportHub>("/hub/activity-import");
+});
+
+app.Run();
+
